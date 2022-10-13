a3ec836d58b0c6b017d3a8b347c8e0efcf129a1c
Maximilian Kalus <info@auxnet.de>
2022-10-13 10:07:49 +0200

    Add version and copyright stuff. Also add `py.typed` file.

--- sitt/__init__.py
+++ sitt/__init__.py
@@ -1,3 +1,28 @@
 # SPDX-FileCopyrightText: 2022-present Maximilian Kalus <info@auxnet.de>
 #
 # SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+import logging
+import sys
+
+from sitt.__about__ import (
+    __author__,
+    __copyright__,
+    __version__,
+)
+
+__all__ = [
+    "__version__",
+    "__author__",
+    "__copyright__",
+]
+
+logger: logging.Logger = logging.getLogger()
+
+# Minimum version check
+python_version: tuple[int] = sys.version_info[:2]
+if python_version[0] < 3 or (python_version[0] == 3 and python_version[1] < 10):
+    logger.critical("Your Python version is too old. Si.T.T. requires at least Python 3.10.")
+    sys.exit(-1)
