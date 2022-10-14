85b94ae26a88e6827126727c8b7908029763350b
Maximilian Kalus <info@auxnet.de>
2022-10-14 11:58:27 +0200

    Bump dev version to 0.0.1.dev1, show version in command line.

--- sitt/cl.py
+++ sitt/cl.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 import logging
 from argparse import ArgumentParser, BooleanOptionalAction, Namespace
 
-from sitt import Configuration, Core
+from sitt import Configuration, Core, __version__
 from sitt.loaders import load_configuration_from_yaml, config_class_loader
 
 
@@ -18,7 +18,7 @@ def parse_params() -> Namespace:
     :return:
     """
     # see: https://docs.python.org/3/library/argparse.html
-    parser = ArgumentParser()
+    parser = ArgumentParser(description='Si.T.T. (Simulation of Traffic and Transport)')
 
     parser.add_argument("-c", "--config", help="Configuration filename", required=True, type=str)
 
@@ -34,6 +34,7 @@ def parse_params() -> Namespace:
     skip_steps.add_argument("--skip-simulation", action=BooleanOptionalAction, help="Skip simulation (and output)",
                             type=bool)
     skip_steps.add_argument("--skip-output", action=BooleanOptionalAction, help="Skip output", type=bool)
+    parser.add_argument('-V', '--version', action='version', version="%(prog)s (" + __version__ + ")")
 
     return parser.parse_args()
 
@@ -62,7 +63,7 @@ def run():
     # create configuration from command line arguments
     config = create_config_from_command_line()
 
-    logging.getLogger().info("Si.T.T. command line - running core.")
+    logging.getLogger().info("Si.T.T. command line - running core (" + __version__ + ").")
 
     # Create and run a Si.T.T. core
     core = Core(config)
