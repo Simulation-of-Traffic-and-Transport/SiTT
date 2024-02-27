05d62b741237174397adb23894fea06c03e0a035
Maximilian Kalus <info@auxnet.de>
2024-02-27 20:56:05 +0100

    Generic geo data xyz converter for all shapes.

--- precalculation/import_hubs_roads.py
+++ precalculation/import_hubs_roads.py
@@ -4,17 +4,8 @@
 """Import hubs and roads from another database."""
 
 from urllib import parse
-import igraph as ig
-import pyproj
-import shapely.ops as sp_ops
-from extremitypathfinder import PolygonEnvironment
-from geoalchemy2 import Geometry
-from pyproj import Transformer
-from shapely import wkb, get_parts, prepare, destroy_prepared, is_ccw, \
-    delaunay_triangles, contains, overlaps, intersection, STRtree, LineString, Polygon, MultiPolygon, Point, \
-    relate_pattern, centroid, shortest_line
-from sqlalchemy import Connection, create_engine, Table, Column, literal_column, insert, schema, MetaData, \
-    Integer, Boolean, String, Float, select, text, func, delete
+
+from sqlalchemy import create_engine, text
 
 # This is an example file - we will interpret data created by archeologists and put it into out sitt schema.
 
@@ -24,6 +15,8 @@ db_password = '12345'
 db_host = 'localhost'
 db_port = 5432
 db_name = 'sitt'
+
+
 # ----------------------------------------
 
 
@@ -35,7 +28,8 @@ def parse_yes_no_entry(s: str) -> bool:
     return False
 
 
-conn = create_engine('postgresql://' + db_user + ':' + parse.quote_plus(db_password) + '@' + db_host + ':' + str(db_port) + '/' + db_name).connect()
+conn = create_engine('postgresql://' + db_user + ':' + parse.quote_plus(db_password) + '@' + db_host + ':' + str(
+    db_port) + '/' + db_name).connect()
 
 # --------------- get hub data from source ---------------
 for result in conn.execute(text("SELECT rechubid, geom, harbor, overnight FROM topology.rechubs")):
@@ -46,7 +40,8 @@ for result in conn.execute(text("SELECT rechubid, geom, harbor, overnight FROM t
     overnight = parse_yes_no_entry(result[3])
     market = False  # just take fixed value for now
 
-    conn.execute(text(f"INSERT INTO sitt.hubs (id, geom, overnight, harbor, market) VALUES ('{hub_id}', '{geom}', {overnight}, {harbor}, {market});"))
+    conn.execute(text(
+        f"INSERT INTO sitt.hubs (id, geom, overnight, harbor, market) VALUES ('{hub_id}', '{geom}', {overnight}, {harbor}, {market});"))
 
 conn.commit()
 
@@ -59,6 +54,7 @@ for result in conn.execute(text("SELECT recroadid, hubaid, hubbid, geom FROM top
     geom = result[3]
     market = False  # just take fixed value for now
 
-    conn.execute(text(f"INSERT INTO sitt.roads (id, geom, hub_id_a, hub_id_b) VALUES ('{road_id}', '{geom}', '{hubaid}', '{hubbid}');"))
+    conn.execute(text(
+        f"INSERT INTO sitt.roads (id, geom, hub_id_a, hub_id_b) VALUES ('{road_id}', '{geom}', '{hubaid}', '{hubbid}');"))
 
 conn.commit()
---
+++ precalculation/make_geo_data_xyz.py
@@ -0,0 +1,151 @@
+# SPDX-FileCopyrightText: 2023-present Maximilian Kalus <info@auxnet.de>
+#
+# SPDX-License-Identifier: MIT
+"""Take a GeoTIFF and create heights for all coordinates in the database."""
+import argparse
+import os
+import sys
+from urllib import parse
+
+import geopandas as gpd
+import rasterio
+from pyproj import Transformer
+from shapely.geometry import shape
+from sqlalchemy import create_engine, Table, Column, MetaData, \
+    String, select, text, update
+
+
+def work_coordinates(coords) -> tuple[list, bool]:
+    changed = False
+    geom = []
+
+    for coord in coords:
+        # keep?
+        if args.keep_existing and len(coord) > 2 and coord[2] > 0:
+            continue
+
+        lng = coord[0]
+        lat = coord[1]
+
+        xx, yy = transformer.transform(lng, lat)
+        x, y = rds.index(xx, yy)
+        height = band[x, y]
+
+        geom.append((lng, lat, height))
+        changed = True
+
+    return geom, changed
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(
+        description="Take a GeoTIFF and create heights for all coordinates in the database.",
+        exit_on_error=False)
+
+    parser.add_argument('-H', '--server', dest='server', default='localhost', type=str, help='database server')
+    parser.add_argument('-D', '--database', dest='database', default='sitt', type=str, help='path to database')
+    parser.add_argument('-U', '--user', dest='user', default='postgres', type=str, help='postgres user')
+    parser.add_argument('-P', '--password', dest='password', default='postgres', type=str, help='postgres password')
+    parser.add_argument('-p', '--port', dest='port', default=5432, type=int, help='postgres port')
+    parser.add_argument('--schema', dest='schema', default='sitt', type=str, help='schema name')
+
+    parser.add_argument('--crs', dest='crs_no', default=4326, type=int, help='projection source')
+    parser.add_argument('--xy', dest='always_xy', default=True, type=bool, help='use the traditional GIS order')
+    parser.add_argument('-i', '--input-file', dest='file', required=True, type=str, help='input file (GeoTIFF)')
+    parser.add_argument('-b', '--band', dest='band', default=1, type=int, help='band to use from GeoTIFF')
+    parser.add_argument('-k', '--keep', dest='keep_existing', default=False, type=bool,
+                        help='keep existing coordinates (overwrite otherwise)')
+
+    parser.add_argument('-t', '--tables', dest='tables', type=str, nargs='+', default='all', help='tables to update',
+                        choices=['all', 'hubs', 'roads', 'water_bodies', 'water_lines'])
+
+    # parse or help
+    args: argparse.Namespace | None = None
+
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        parser.exit(1)
+
+    # check file exists
+    if not os.path.exists(args.file):
+        print("File does not exist: " + args.file)
+        parser.exit(1)
+
+    # load geo
+    rds: rasterio.io.DatasetReader = rasterio.open(os.path.abspath(args.file))
+    transformer = Transformer.from_crs(args.crs_no, rds.crs, always_xy=args.always_xy)
+    # get relevant band
+    band = rds.read(args.band)
+
+    # which tables should be updated
+    tables = args.tables
+    if tables == 'all' or 'all' in tables:
+        tables = ['hubs', 'roads', 'water_bodies', 'water_lines']
+
+    # connect to database
+    conn = create_engine('postgresql://' + args.user + ':' + parse.quote_plus(args.password) + '@' + args.server + ':' +
+                         str(args.port) + '/' + args.database).connect()
+
+    print("GeoTIFF loaded, band loaded, database connected - working...")
+
+    for table in tables:
+        print("Updating " + table)
+
+        # get hubs - create statement via sql alchemy
+        idCol = Column('id')
+        geomCol = Column('geom')
+        t = Table(table, MetaData(), idCol, geomCol, schema=args.schema)
+        s = select(idCol, geomCol).select_from(t)
+        data = gpd.GeoDataFrame.from_postgis(str(s.compile()), conn,
+                                             geom_col='geom',
+                                             index_col='id')
+
+        counter = 0
+        total = len(data)
+
+        for idx, row in data.iterrows():
+            if 'geom' in row:
+                g = row.geom
+                changed_any = False
+
+                # complex shapes, like Polygons, MultiPolygons
+                if g.geom_type == 'Polygon' or g.geom_type == 'MultiPolygon':
+                    all_coords = []
+
+                    # get coordinates for exterior ring
+                    new_coords, changed = work_coordinates(g.exterior.coords)
+                    all_coords.append(new_coords)
+                    if changed:
+                        changed_any = True
+
+                    for interior in g.interiors:
+                        new_coords, changed = work_coordinates(interior.coords)
+                        all_coords.append(new_coords)
+                        if changed:
+                            changed_any = True
+
+                    if changed_any:
+                        # create SQL statement
+                        new_shape = shape({"type": row.geom.geom_type, "coordinates": all_coords})
+
+                # simple shapes, like Points, etc.
+                elif g and g.coords:
+                    new_coords, changed_any = work_coordinates(g.coords)
+                    new_shape = shape({"type": row.geom.geom_type, "coordinates": new_coords})
+
+                # any change?
+                if changed_any:
+                    # create SQL statement
+                    new_value = text(String().literal_processor(dialect=conn.dialect)(
+                        value="SRID=" + str(args.crs_no) + ";" + str(new_shape)))
+                    stmt = update(t).where(idCol == row.name).values(geom=new_value)
+                    conn.execute(stmt)
+                    counter += 1
+
+        conn.commit()
+        print(f"Updated {counter}/{total} coordinates in {table}.")
+
+print("Done.")
