97ab0f2a00cc52261ea490532bf259cb183cc76e
Maximilian Kalus <info@auxnet.de>
2024-12-04 13:01:56 +0100

    Add simple loading state for reloading stuff in a fixed time when changing path types, e.g. road to river.

---
+++ sitt/modules/simulation_define_state/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2022-present Maximilian Kalus <info@auxnet.de>
+#
+# SPDX-License-Identifier: MIT
+"""Simulation Define State Classes"""
+
+from .simple_loading_state import SimpleLoadingState
+
+__all__ = [
+    "SimpleLoadingState",
+]
---
+++ sitt/modules/simulation_define_state/simple_loading_state.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2022-present Maximilian Kalus <info@auxnet.de>
+#
+# SPDX-License-Identifier: MIT
+"""
+SimpleLoadingState will add a fixed amount of time to the agent's current time when the route type changes, e.g. from
+road to river.
+"""
+import logging
+
+from sitt import SimulationDefineStateInterface, Configuration, Context, State, Agent
+
+logger = logging.getLogger()
+
+class SimpleLoadingState(SimulationDefineStateInterface):
+    """
+    SimpleLoadingState will add a fixed amount of time to the agent's current time when the route type changes, e.g.
+    from road to river.
+    """
+    def __init__(self, add_time: float = 0.5):
+        super().__init__()
+        self.add_time: float = add_time
+
+    def define_state(self, config: Configuration, context: Context, agent: Agent) -> State:
+        state = agent.state
+
+        # ignore, if there is no last route
+        if agent.last_route is None or agent.last_route == '':
+            return state
+
+        # get route types
+        last_route_type = context.get_path_by_id(agent.last_route)['type']
+        current_route_type = context.get_path_by_id(agent.route_key)['type']
+
+        # types are not equal => add time to consider reloading goods from one vehicle type to another
+        if last_route_type != current_route_type:
+            agent.current_time += self.add_time
+
+            if not self.skip and logger.level <= logging.DEBUG:
+                logger.debug(f"SimpleLoadingState: reloading due to route type change, new time = {agent.current_time:.2f} hours")
+
+        return state
