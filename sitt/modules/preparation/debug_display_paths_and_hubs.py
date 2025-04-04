469ae6c3e00e1d7d8fb86bb5a80d0d8e844b588e
Maximilian Kalus <info@auxnet.de>
2025-04-04 10:39:41 +0200

    Started working on new directed graph settings.

--- sitt/modules/preparation/debug_display_paths_and_hubs.py
+++ sitt/modules/preparation/debug_display_paths_and_hubs.py
@@ -13,16 +13,17 @@ import yaml
 from shapely.geometry import GeometryCollection
 
 from sitt import Configuration, Context, PreparationInterface
+from sitt.convert_graph_to_directed import convert_graph_to_directed
+import shapefile
 
 logger = logging.getLogger()
 
 
 class DebugDisplayPathsAndHubs(PreparationInterface):
-    def __init__(self, draw_network: bool = True, show_network: bool = True, save_network: bool = False,
+    def __init__(self, route: str | None = None, draw_network: bool = True, show_network: bool = True, save_network: bool = False,
                  save_network_name: str = 'network', save_network_type: str = 'png', display_routes: bool = True,
-                 start: str | None = None, end: str | None = None, show_graphs: bool = True, save_graphs: bool = False,
-                 save_graphs_names: str = 'possible_routes', save_graphs_type: str = 'png',
-                 save_graphs_number: int = 5):
+                 start: str | None = None, end: str | None = None, save_shapefile: bool = False,
+                 save_shapefile_name: str = 'network'):
         super().__init__()
         self.draw_network: bool = draw_network
         """draw the network graph"""
@@ -38,124 +39,119 @@ class DebugDisplayPathsAndHubs(PreparationInterface):
         self.start: str | None = start
         """start hub id of example route"""
         self.end: str | None = end
+        """Route to be considered - required"""
+        self.route: str | None = route
         """end hub id of example route"""
-        self.show_graphs: bool = show_graphs
-        """plot routes to stdout"""
-        self.save_graphs: bool = save_graphs
-        """Save all graphs to disk"""
-        self.save_graphs_names: str = save_graphs_names
-        self.save_graphs_type: str = save_graphs_type
-        """possible values are eps, jpeg, jpg, pdf, pgf, png, ps, raw, rgba, svg, svgz, tif, tiff"""
-        self.save_graphs_number: int = save_graphs_number
-        """number of graphs to save/show"""
+        self.save_shapefile: bool = save_shapefile
+        """save network as shapefile"""
+        self.save_shapefile_name: str = save_shapefile_name
+        """Name of the shapefile to save"""
 
     def run(self, config: Configuration, context: Context) -> Context:
-        if context.graph:
+        if self.route is None:
+            logger.error("No route specified.")
+            return context
+
+        # ### TODO
+        #
+        # g = convert_graph_to_directed(context.graph, self.route, False, self.start, self.end)
+        # fig, ax = plt.subplots()
+        # fig.set_dpi(300)
+        #
+        # # coordinates for nodes
+        # v_x = []
+        # v_y = []
+        #
+        # for vs in g.vs:
+        #     v_x.append(vs['geom'].coords[0][0])
+        #     v_y.append(vs['geom'].coords[0][1])
+        #
+        # p = gpd.GeoSeries(gpd.points_from_xy(x=v_x, y=v_y))
+        # p.plot(ax=ax,markersize=1)
+        # plt.title(self.route)
+        #
+        # for es in g.es:
+        #     s = es.source_vertex['geom']
+        #     t = es.target_vertex['geom']
+        #     ax.annotate(text='', xytext=(s.x, s.y), xy=(t.x, t.y), arrowprops=dict(arrowstyle='->', shrinkB=0, shrinkA=0, mutation_scale=4))
+        #
+        # plt.show()
+        #
+        # ### TODO
+        # exit(0)
+
+        # convert route to lower case for case-insensitive comparison
+        route = self.route.lower()
+
+        if context.graph and self.draw_network:
             logger.info("Displaying paths and hubs")
 
-            # draw complete network
-            if self.draw_network:
-                logger.info("Drawing network.")
+            vertices: set[str] = set()
 
-                # coordinates for nodes
-                v_x = []
-                v_y = []
-                geometries = []
+            fig, ax = plt.subplots()
 
-                for vs in context.graph.vs:
-                    v_x.append(vs['geom'].coords[0][0])
-                    v_y.append(vs['geom'].coords[0][1])
+            for es in context.graph.es:
+                if 'directions' in es.attribute_names() and route in es['directions'] and es['directions'][route] != 0:
+                    dir = es['directions'][route]
 
-                for es in context.graph.es:
-                    geometries.append(es['geom'])
+                    s = context.graph.vs.find(name=es['from']) # we can trust this value
+                    t = context.graph.vs.find(name=es['to']) # we can trust this value
 
-                _, ax = plt.subplots()
+                    vertices.add(s.index)
+                    vertices.add(t.index)
 
-                lines = gpd.GeoSeries(GeometryCollection(geometries))
-                lines.plot(ax=ax, color='coral')
+                    arrow_style = '->'
+                    if dir == -1:
+                        arrow_style = '<-'
+                    if dir == 2:
+                        arrow_style = '<->'
 
-                p = gpd.GeoSeries(gpd.points_from_xy(x=v_x, y=v_y))
-                p.plot(ax=ax)
+                    ax.annotate(text='', xytext=(s['geom'].x, s['geom'].y), xy=(t['geom'].x, t['geom'].y), arrowprops=dict(arrowstyle=arrow_style, shrinkB=0, shrinkA=0, mutation_scale=4))
 
-                if self.show_graphs:
-                    plt.show()
+            # coordinates for nodes
+            v_x = []
+            v_y = []
 
-                if self.save_network:
-                    plt.savefig('%s.%s' % (self.save_network_name, self.save_network_type),
-                                bbox_inches='tight', dpi=150)
+            for vs in context.graph.vs.select(list(vertices)):
+                v_x.append(vs['geom'].coords[0][0])
+                v_y.append(vs['geom'].coords[0][1])
 
-            # draw single legs from a to b
-            if self.display_routes and self.start and self.end:
-                counter = 0
-                paths = []
-                for p in context.graph.get_k_shortest_paths(self.start, to=self.end, k=self.save_graphs_number,
-                                                            weights='length_m', mode='all', output='epath'):
-                    paths.append(p)
-                if logger.level <= logging.INFO:
-                    logger.info("Drawing %d route(s) from %s to %s.", len(paths), self.start, self.end)
+            p = gpd.GeoSeries(gpd.points_from_xy(x=v_x, y=v_y))
+            p.plot(ax=ax,markersize=2)
+            plt.title(self.route)
 
-                for path in paths:
-                    # for plotting the height profile
-                    profile_legs: np.darray = np.zeros((1,))  # first leg point starts at point 0
-                    profile_height: np.darray | None = None
-                    total_length = 0.0
-                    geometries = []
+            if self.show_network:
+                plt.show()
 
-                    # get single legs
-                    for edge_id in path:
-                        # third entry in tuple is the id of the vertex
-                        edge = context.graph.es[edge_id]
-                        total_length += edge['length_m']
+            if self.save_network:
+                fig.savefig('%s.%s' % (self.save_network_name, self.save_network_type),
+                            bbox_inches='tight', dpi=300)
 
-                        # add leg points
-                        my_legs = edge['legs']
+        # ### TODO
+        if context.graph and self.save_shapefile:
+            logger.info("Saving network as shapefile")
 
-                        offset = len(profile_legs)
-                        my_len = len(my_legs)
-                        profile_legs = np.append(profile_legs, np.zeros((my_len,)))
-                        for i in range(0, my_len):
-                            profile_legs[offset + i] = profile_legs[offset + i - 1] + my_legs[i]
+            w = shapefile.Writer(target=self.save_shapefile_name, shapeType=shapefile.POLYLINE,
+                                 autoBalance=True)
+            w.field("name", "C")
+            w.field("from", "C")
+            w.field("to", "C")
 
-                        # add heights
-                        my_coords = edge['geom'].coords
-                        geom_len = len(my_coords)
-                        my_heights = np.zeros((geom_len,))
-                        for i in range(0, geom_len, 1):
-                            my_heights[i] = my_coords[i][2]
+            for es in context.graph.es:
+                if 'directions' in es.attribute_names() and route in es['directions'] and es['directions'][route] != 0:
+                    dir = es['directions'][route]
 
-                        if profile_height is not None:
-                            # delete first height
-                            profile_height = np.append(profile_height, np.delete(my_heights, 0))
-                        else:
-                            profile_height = my_heights
+                    s = context.graph.vs.find(name=es['from']) # we can trust this value
+                    t = context.graph.vs.find(name=es['to']) # we can trust this value
 
-                        geometries.append(edge['geom'])
+                    if dir == 1 or dir == 2:
+                        w.line([[[s['geom'].x, s['geom'].y], [t['geom'].x, t['geom'].y]]])
+                        w.record(es['name'], s['name'], t['name'])
+                    if dir == -1 or dir == 2:
+                        w.line([[[t['geom'].x, t['geom'].y], [s['geom'].x, s['geom'].y]]])
+                        w.record(es['name'] + '_rev', t['name'], s['name'])
 
-                    counter += 1
-
-                    # geographic view of route
-                    fig, (col1, col2) = plt.subplots(2, 1)
-                    fig.tight_layout(pad=2)
-                    p = gpd.GeoSeries(GeometryCollection(geometries))
-                    p.plot(ax=col1)
-                    col1.set_title("%s to %s, Route %d" % (self.start, self.end, counter))
-
-                    # height profile of route
-                    plt.plot(profile_legs, profile_height,
-                             color='red')
-                    plt.title("Height Profile")
-                    plt.xlabel("Distance (m, total = %f m)" % total_length)
-                    plt.ylabel("Height (m)")
-
-                    if self.show_graphs:
-                        plt.show()
-
-                    if self.save_graphs:
-                        fig.savefig('%s_%d.%s' % (self.save_graphs_names, counter, self.save_graphs_type),
-                                    bbox_inches='tight', dpi=150)
-
-        else:
-            logger.info("Skipping display of paths and hubs - no data.")
+            w.close()
 
         return context
 
