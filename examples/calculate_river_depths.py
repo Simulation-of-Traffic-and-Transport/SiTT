614c076b0c86e461007fd73869d5fcd5619f551e
Maximilian Kalus <info@auxnet.de>
2025-05-20 15:36:39 +0200

    Restructure code, so it works for disjunct river routes and interpolates without errors.

--- examples/calculate_river_depths.py
+++ examples/calculate_river_depths.py
@@ -10,9 +10,11 @@ import argparse
 
 import geopandas as gpd
 import numpy as np
+import pandas as pd
+from geopandas import GeoDataFrame
 from matplotlib.tri import Triangulation, LinearTriInterpolator
 from shapely import wkb, LineString
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, bindparam
 
 if __name__ == "__main__":
     # parse arguments
@@ -61,51 +63,96 @@ if __name__ == "__main__":
                 f"ALTER TABLE {args.river_table} ADD {args.river_depths_column} double precision[]"))
             conn.commit()
 
-    # Load depths data
-    gdf = gpd.GeoDataFrame.from_postgis(
-        f"SELECT * FROM topology.river_depths", conn, geom_col='geom')
-    totalPointsArray = np.zeros([gdf.shape[0], 3])
+    # Group river data - we have point clouds per river, but which shapes belong to the same river/route system?
+    # First, get all unique shapes
+    print("Constructing shape groups...")
+    shape_data = []
+    # get all unique shapes
+    for shape in conn.execute(text(f"SELECT DISTINCT shape FROM topology.river_depths")):
+        # get convex hull of this shape's points - i.e. we have a polygon that contains all points of this shape
+        hull = wkb.loads(conn.execute(text(f"select ST_CONVEXHULL(ST_COLLECT(geom)) from topology.river_depths where shape = '{shape[0]}'")).fetchone()[0])
+        shape_data.append((shape[0], hull,))
 
-    for index, p in gdf.iterrows():
-        pointArray = np.array([p.geom.coords.xy[0][0], p.geom.coords.xy[1][0], p['depth']])
-        totalPointsArray[index] = pointArray
+    # convert to Geopandas DataFrame containing shape names and convex hulls
+    shape_data = gpd.GeoDataFrame(shape_data, columns=['shape', 'hull'], geometry='hull')
 
-    # triangulation function
-    triFn = Triangulation(totalPointsArray[:, 0], totalPointsArray[:, 1])
-    linTriFn = LinearTriInterpolator(triFn, totalPointsArray[:, 2])
+    # traverse rivers - we need to identify groups of rivers in order to group the triangulations
+    print("Grouping rivers...")
+    shape_groups: dict[frozenset[int], list[str]] = {}
 
-    # traverse river data
     for data in conn.execute(text(f"select {args.river_id_column}, {args.river_geo_column} from {args.river_table} WHERE {args.river_depths_column} IS NULL")):
-        # load wkb data
         geom = wkb.loads(data[1])
-        # structure to add data to
-        coords = np.array(geom.coords)
-        # structure to add data to
-        new_geom = np.array(geom.coords)
-        # z column exists?
-        if new_geom.shape[1] < 3:
-            new_geom = np.c_[new_geom, np.zeros(len(new_geom))]
+        # get intersecting shapes
+        df: pd.Series = shape_data.intersects(geom)
+        mask = df.values == True  # create mask to filter out only intersecting shapes
+        # convert intersecting shapes to frozen set of indexes
+        in_shapes = frozenset(pd.Series(df.values[mask], df.index[mask]).index.tolist())
+        if len(in_shapes) == 0:
+            print(f"No intersecting depth shapes for river {data[0]}")
+            continue
 
-        # this will hold the depths for each point
-        heights = np.zeros(len(coords))
+        # add to groups
+        if in_shapes not in shape_groups:
+            shape_groups[in_shapes] = []
+        shape_groups[in_shapes].append(data[0])
 
-        try:
-            # interpolate depths for each point in the river line
-            for idx, coord in enumerate(coords):
-                tempZ = linTriFn(coord[0],coord[1])
-                if np.isnan(float(tempZ)):
-                    # nans are converted to 0s
-                    tempZ = np.float64(0.)
-                heights[idx]=tempZ
-                new_geom[idx, 2] = tempZ
+    print(f"Created {len(shape_groups)} shape groups. Working in them...")
 
-            print("OK:", data[0])
-        except:
-            print("FAIL:", data[0])
+    # now do actual work per interpolation group: interpolate data...
+    for idx, shapes in shape_groups.items():
+        # get shape names of this group
+        shape_names = shape_data.loc[list(idx)]['shape'].tolist()
 
-        heights_str = "{" + list(heights).__str__()[1:-1] + "}"
-        updated_geom = LineString(new_geom).wkt
-        conn.execute(text(f"UPDATE {args.river_table} SET {args.river_depths_column} = '{heights_str}', {args.river_geo_column} = ST_GeomFromText('{updated_geom}') WHERE {args.river_id_column} = '{data[0]}'"))
+        print(f"Interpolating depths for group {shape_names} containing {len(shapes)} routes...")
+
+        # Load depths data
+        sql = f"SELECT * FROM topology.river_depths WHERE shape IN ('{"', '".join(shape_names)}')"
+        gdf = gpd.GeoDataFrame.from_postgis(sql, conn, geom_col='geom')
+        totalPointsArray = np.zeros([gdf.shape[0], 3])
+
+        for index, p in gdf.iterrows():
+            pointArray = np.array([p.geom.coords.xy[0][0], p.geom.coords.xy[1][0], p['depth']])
+            totalPointsArray[index] = pointArray
+
+        # triangulation function
+        triFn = Triangulation(totalPointsArray[:, 0], totalPointsArray[:, 1])
+        linTriFn = LinearTriInterpolator(triFn, totalPointsArray[:, 2])
+
+
+        print("Start calculating depths for this group...")
+
+        # traverse river data
+        for data in conn.execute(text(f"select {args.river_id_column}, {args.river_geo_column} from {args.river_table} WHERE {args.river_depths_column} IS NULL AND {args.river_id_column} IN :ids").bindparams(bindparam(key="ids", value=shapes, expanding=True))):
+            # load wkb data
+            geom = wkb.loads(data[1])
+            # structure to add data to
+            coords = np.array(geom.coords)
+            # structure to add data to
+            new_geom = np.array(geom.coords)
+            # z column exists?
+            if new_geom.shape[1] < 3:
+                new_geom = np.c_[new_geom, np.zeros(len(new_geom))]
+
+            # this will hold the depths for each point
+            heights = np.zeros(len(coords))
+
+            try:
+                # interpolate depths for each point in the river line
+                for idx, coord in enumerate(coords):
+                    tempZ = linTriFn(coord[0],coord[1])
+                    if np.isnan(float(tempZ)):
+                        # nans are converted to 0s
+                        tempZ = np.float64(0.)
+                    heights[idx]=tempZ
+                    new_geom[idx, 2] = tempZ
+
+                print("OK:", data[0])
+            except:
+                print("FAIL:", data[0])
+
+            heights_str = "{" + list(heights).__str__()[1:-1] + "}"
+            updated_geom = LineString(new_geom).wkt
+            conn.execute(text(f"UPDATE {args.river_table} SET {args.river_depths_column} = '{heights_str}', {args.river_geo_column} = ST_GeomFromText('{updated_geom}') WHERE {args.river_id_column} = '{data[0]}'"))
 
     # persist changes to the database
     conn.commit()
