278603b3a242982035d24e2bdfba1bf714687368
Maximilian Kalus <info@auxnet.de>
2025-06-17 08:55:46 +0200

    New ERA5 data and document variables.

--- sitt/modules/preparation/load_data_from_netcdf.py
+++ sitt/modules/preparation/load_data_from_netcdf.py
@@ -14,26 +14,60 @@ Example YAML could be:
       longitude: longitude
       time: time
       variables:
+        # Full table: https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation
         temperature:
+        # 2 metre temperature in °K, converted into °C
+        # 2m_temperature
+        # https://codes.ecmwf.int/grib/param-db/167
           variable: t2m
           offset: -273.15
+        # Convective rain rate in kg/(m²s)
+        # convective_rain_rate
+        # https://codes.ecmwf.int/grib/param-db/228218
         rainfall:
           variable: crr
+        # Convective snowfall rate water equivalent in kg/(m²s)
+        # convective_snowfall_rate_water_equivalent
+        # https://codes.ecmwf.int/grib/param-db/228220
         snowfall:
           variable: csfr
+        # Snow depth in m of water equivalent
+        # snow_depth
+        # https://codes.ecmwf.int/grib/param-db/141
         snow_depth:
           variable: sd
+        # Precipitation type in integer type (0: no precipitation, 1: rain, 5: snow, ...)
+        # precipitation_type
+        # https://codes.ecmwf.int/grib/param-db/260015
+        precipitation_type:
+          variable: ptype
+        # 10 metre V (northward) wind component in m/s
+        # 10m_v_component_of_wind
+        # https://codes.ecmwf.int/grib/param-db/166
+        v10:
+          variable: v10
+        # 10 metre U (eastward) wind component in m/s
+        # 10m_u_component_of_wind
+        # https://codes.ecmwf.int/grib/param-db/165
+        u10:
+          variable: u10
+        # K index (potential for a thunderstorm) in K (see table)
+        # k_index
+        # https://codes.ecmwf.int/grib/param-db/260121
+        k_index:
+          variable: kx
 
 See information on this dataset: https://cds.climate.copernicus.eu/datasets/reanalysis-era5-single-levels
 and documentation: https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation
 """
 
+import datetime as dt
 import logging
 
 from netCDF4 import Dataset
 
 from sitt import Configuration, Context, PreparationInterface, SpaceTimeData
-import datetime as dt
+
 logger = logging.getLogger()
 
 
@@ -67,8 +101,14 @@ class LoadDataFromNETCDF(PreparationInterface):
         if logger.level <= logging.INFO:
             logger.info("Loading NETCDF file: " + self.filename)
 
-        context.space_time_data[self.name] = SpaceTimeData(Dataset(self.filename, 'r', format=self.file_format),
-                                                           self.variables, self.latitude, self.longitude, self.time,
-                                                           self.start_date)
+        dataset = Dataset(self.filename, 'r', format=self.file_format)
+
+        # show data available in the netcdf file
+        if logger.level <= logging.DEBUG:
+            logger.debug("Variables in dataset: " + ', '.join(dataset.variables.keys()))
+
+        # create space_time_data object
+        context.space_time_data[self.name] = SpaceTimeData(dataset, self.variables, self.latitude, self.longitude,
+                                                           self.time, self.start_date)
 
         return context
