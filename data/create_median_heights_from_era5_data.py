9bef79ec03b5e1b1a76f4ba49fe5ec02a74f5cb4
Maximilian Kalus <info@auxnet.de>
2025-07-25 10:55:07 +0200

    When creating median heights from ERA5 data, create mean and modus heights, too.

--- data/create_median_heights_from_era5_data.py
+++ data/create_median_heights_from_era5_data.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Create median heights from era5 data
+# Create mean/median/mode heights from era5 data
 
 import numpy as np
 import rasterio
 import rioxarray
 import xarray as xr
+from scipy import stats
 
 # Open the heights raster TIFF file and reproject it to EPSG:4326, so it is compatible with the ERA5 data
 raster = rioxarray.open_rasterio('DTM Austria 10m v2 by Sonny.tif', masked=True).squeeze()
@@ -24,6 +25,8 @@ diff_lon = np.abs(lons[1] - lons[0])/2
 
 # create a new dataset that will store the mean heights
 mean_heights = np.zeros((len(lats), len(lons)))
+median_heights = np.zeros((len(lats), len(lons)))
+mode_heights = np.zeros((len(lats), len(lons)))
 
 # now calculate median height for each pixel in the new dataset
 for i in range(len(lats)):
@@ -39,24 +42,42 @@ for i in range(len(lats)):
         try:
             # clip raster to current pixel bounding box
             data = raster_new.rio.clip_box(minx=min_x, maxx=max_x, miny=min_y, maxy=max_y)
-            # calculate and store median height for current pixel - it is the mean of all non-NaN values in the clipped raster
-            h = float(data.median().values)
+            # calculate and store mean height for current pixel - it is the mean of all non-NaN values in the clipped raster
+            mh = float(data.mean().values)
+            # calculate and store median height for current pixel
+            md = float(data.median().values)
+            # calculate and store mode height for current pixel - we flatten the values, find the lowest common height band (100 m resolution) and find the most common one within this raster. We add 50 m to get the middle value.
+            result = stats.mode(np.floor(data.values.flatten() / 100) * 100, nan_policy='omit').mode
+            if not np.isnan(result):
+                mm = result + 50
+            else:
+                mm = np.nan
         except:
             # some of the pixels in the raster may not cover the current pixel bounding box, so we set the median height to NaN
-            h = np.nan
+            mh = np.nan
+            md = np.nan
+            mm = np.nan
 
-        # store the median height in the new dataset if it is not NaN
-        if not np.isnan(h):
-            mean_heights[i, j] = h
+        # store the median/mean/modus height in the new dataset if it is not NaN
+        if not np.isnan(mh):
+            mean_heights[i, j] = mh
+
+        if not np.isnan(md):
+            median_heights[i, j] = md
+
+        if not np.isnan(mm):
+            mode_heights[i, j] = mm
 
             #print(f"Height of pixel ({j}, {i}) is {h:.2f} m")
 
-# save the new dataset as a netCDF file - we create an xarray with one variable and coordinates
-mean_heights_data = xr.Dataset(
+# save the new dataset as a netCDF file - we create an xarray with variables and coordinates
+heights_data = xr.Dataset(
     data_vars=dict(
-        mean_height=(["latitude", "longitude"], mean_heights)
+        mean_height=(["latitude", "longitude"], mean_heights),
+        median_height=(["latitude", "longitude"], median_heights),
+        mode_height=(["latitude", "longitude"], mode_heights),
     ),
     coords={'latitude': lats, 'longitude': lons}
 )
 
-mean_heights_data.to_netcdf('median_heights_data.nc')
+heights_data.to_netcdf('mean_heights_data.nc')
