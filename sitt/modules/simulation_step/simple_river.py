6eddee563b61e25708de718c08e51bb1f64dd4a6
Maximilian Kalus <info@auxnet.de>
2025-12-08 17:03:28 +0100

    Use SimpleDAV for upstream towing.

--- sitt/modules/simulation_step/simple_river.py
+++ sitt/modules/simulation_step/simple_river.py
@@ -8,12 +8,14 @@ import igraph as ig
 import yaml
 
 from sitt import Configuration, Context, Agent, State, SimulationStepInterface
+from sitt.modules.simulation_step import SimpleDAV
 
 logger = logging.getLogger()
 
 
 class SimpleRiver(SimulationStepInterface):
-    def __init__(self, min_speed_down: float = 3.35, min_speed_up: float = 3.35):
+    def __init__(self, min_speed_down: float = 3.35, min_speed_up: float = 3.35, upstream_is_dav: bool = True,
+                 ascend_per_hour: float = 300, descend_per_hour: float = 400):
         """
         :param min_speed_down: minimum row speed in kph (if river is slower than this, take this as minimum speed).
         :param min_speed_up: speed in kph when towing upriver. If not provided, defaults to speed.
@@ -21,6 +23,11 @@ class SimpleRiver(SimulationStepInterface):
         super().__init__()
         self.min_speed_down: float = min_speed_down
         self.min_speed_up: float = min_speed_up if min_speed_up >= 0. else self.speed
+        self.upstream_is_dav: bool = upstream_is_dav
+        self.ascend_per_hour: float = ascend_per_hour
+        """m of height per hour while ascending - upstream"""
+        self.descend_per_hour: float = descend_per_hour
+        """m of height per hour while descending - upstream"""
 
     def update_state(self, config: Configuration, context: Context, agent: Agent, next_leg: ig.Edge) -> State:
         # skipped?
@@ -32,6 +39,11 @@ class SimpleRiver(SimulationStepInterface):
             logger.error(f"SimulationInterface SimpleRiver error, path {agent.route_key} is not a river")
             agent.state.signal_stop_here = True
             return agent.state
+
+        # check upstream river -> this behaves like Simple DAV
+        if self.upstream_is_dav and 'direction' in next_leg.attribute_names() and next_leg['direction'] == 'upwards':
+            dav = SimpleDAV(speed=self.min_speed_up, ascend_per_hour=self.ascend_per_hour, descend_per_hour=self.descend_per_hour)
+            return dav.update_state(config, context, agent, next_leg)
 
         # traverse and calculate time taken for this leg of the journey
         time_taken = 0.
@@ -56,16 +68,14 @@ class SimpleRiver(SimulationStepInterface):
 
             length = next_leg['legs'][i]  # length is in meters
 
-            # river speed - we take this and the next point's flow rate to calculate the speed
-            kph = (flows[i] + flows[i+1])/2  * 3.6
-
             # determine speed
-            if 'direction' in next_leg.attribute_names() and next_leg['direction'] == 'upstream':
-                min_speed = self.min_speed_up
+            if 'direction' in next_leg.attribute_names() and next_leg['direction'] == 'upwards':
+                # this is only used, if self.upstream_is_dav is false
+                current_speed = self.min_speed_up
             else:
-                min_speed = self.min_speed_down
-
-            current_speed = max(min_speed, kph)
+                # river speed - we take this and the next point's flow rate to calculate the speed
+                kph = (flows[i] + flows[i + 1]) / 2 * 3.6
+                current_speed = max(self.min_speed_down, kph)
 
             # calculate time taken in units (hours) for this part
             calculated_time = length / (current_speed * 1000)
