0fe770a8ca2f600f61f78691aefecaea5723f43f
Maximilian Kalus <info@auxnet.de>
2026-02-25 10:16:13 +0100

    Change argument again.

--- precalculation/00_make_xyz.py
+++ precalculation/00_make_xyz.py
@@ -159,7 +159,7 @@ if __name__ == "__main__":
     parser.add_argument('-b', '--band', dest='band', default=1, type=int, help='band to use from GeoTIFF')
     parser.add_argument('-k', '--keep', dest='keep_existing', default=True, type=bool,
                         help='keep existing heights (overwrite otherwise)')
-    parser.add_argument('-S', '--create-segments', dest='create_segments', default=False, type=bool,
+    parser.add_argument('-S', '--create-segments', dest='create_segments', default=True, type=bool,
                         help='create new input coordinates for roads (not rivers) by splitting the line into segments based on height tiles; improves the heights a bit, probably not needed if your input data is pretty good anyway')
     parser.add_argument('--segment-use-hypotenuse', dest='segment_use_hypotenuse', default=False, type=bool,
                         help='to calculate heights for segments using the hypotenuse of the resolution triangle instead of the minimum resolution triangle side (default: false)')
