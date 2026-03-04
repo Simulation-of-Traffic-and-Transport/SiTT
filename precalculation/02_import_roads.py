f5cd3f3a36450aa43455ffc1d637dab2b0bfe833
Maximilian Kalus <info@auxnet.de>
2026-03-04 10:53:08 +0100

    Cut roads into 500m segments for slope calculation.

--- precalculation/02_import_roads.py
+++ precalculation/02_import_roads.py
@@ -6,16 +6,18 @@
 import argparse
 import math
 from urllib import parse
+import os
 
 import geopandas as gpd
 import numpy as np
 from geoalchemy2 import Geometry, WKTElement
 from pyproj import Transformer
-from shapely import force_2d, force_3d, wkb, Point, LineString
-from shapely.ops import transform
+from shapely import force_2d, force_3d, wkb, Point, LineString, MultiPoint
+from shapely.ops import transform, split, snap, nearest_points
 from sqlalchemy import create_engine, Table, Column, MetaData, \
     String, text, insert
 from sqlalchemy.dialects.postgresql import JSONB
+import rasterio
 
 from precalculation.common import parse_yes_no_entry
 
@@ -42,8 +44,11 @@ if __name__ == "__main__":
     parser.add_argument('--geom-field', dest='geom_field', default='geom', type=str, help='field containing geometry (should be 3D)')
 
     parser.add_argument('--correct-directions', dest='correct', default=True, type=bool, help='correct directions if necessary (tests geography of linestring)')
+    parser.add_argument('-s', '--segment-length', dest='segment_length', default=500., type=float, help='maximum length of segments to recalculate roads in (0. to disable segmentation, default 500 meters)')
     parser.add_argument('--consider-heights', dest='consider_heights', default=True, type=bool,
-                        help='calculate heights/slopes into length of path')
+                        help='calculate heights/slopes into length of path (only without segments)')
+    parser.add_argument('-i', '--input-file', dest='file', type=str, help='input file for heights (GeoTIFF) - only needed for segmentation')
+    parser.add_argument('--band', dest='band', default=1, type=int, help='band to use from GeoTIFF')
     parser.add_argument('--delete', dest='delete', default=True, type=bool, help='delete before import')
     parser.add_argument('--drop', dest='drop', default=False, type=bool, help='drop table before import')
 
@@ -61,7 +66,28 @@ if __name__ == "__main__":
         parser.print_help()
         parser.exit(1)
 
+    # load GeoTIFF, if defined
+    if args.segment_length > 0:
+        if args.file:
+            # check file exists
+            if not os.path.exists(args.file):
+                print("File does not exist: " + args.file)
+                parser.exit(1)
+
+            rds: rasterio.io.DatasetReader = rasterio.open(args.file)
+            # get relevant band
+            band = rds.read(args.band)
+            print("Heights loaded from GeoTIFF.")
+
+            if rds.crs != 'EPSG:' + str(args.crs_to):
+                print("Error: GeoTIFF CRS does not match target CRS. Failing...")
+                exit(8)
+        else:
+            print("No GeoTIFF specified.")
+            exit(7)
+
     transformer = Transformer.from_crs(args.crs_from, args.crs_to, always_xy=args.always_xy)
+    transformer_back = Transformer.from_crs(args.crs_to, args.crs_from, always_xy=args.always_xy)
 
     # connect to database
     conn = create_engine('postgresql://' + args.user + ':' + parse.quote_plus(args.password) + '@' + args.server + ':' +
@@ -183,50 +209,141 @@ if __name__ == "__main__":
         # Calculate single legs
         length = 0.
         legs = []  # in m
-        slopes = []  # in percent
+        leg_points = None
+        slopes = []  # in percent (maximum slope if more than one)
+        legs_up = []  # in m
+        legs_down = []  # in m
         base_length = 0.  # in m, for calculating costs
         up_abs = 0.  # in m, for calculating costs
         down_abs = 0.  # in m, for calculating costs
 
-        last_coord = None
-        for coord in geom.coords:
-            if last_coord is not None:
-                # distance calculation for each leg
-                leg = transform(transformer.transform, LineString([last_coord, coord]))
-                leg_length = leg.length
-                base_length += leg.length
+        if args.segment_length > 0.:
+            # into metric space
+            g_m = transform(transformer.transform, force_2d(geom))
 
-                # asc/desc
-                diff = last_coord[2] - coord[2]
+            # how many points do we need to create?
+            points_to_create = math.ceil(g_m.length / args.segment_length)
+            distance_delta = g_m.length / points_to_create
+            distances = np.arange(0, g_m.length, distance_delta)
 
-                # add height to length calculation
-                if args.consider_heights:
-                    leg_length = np.sqrt([leg_length * leg_length + diff * diff])[0]
+            # create points along the road
+            points_m = MultiPoint([g_m.interpolate(distance) for distance in distances] + [Point(g_m.coords[-1])])
 
-                # logger.info("%f, %f", diff, leg_length)
-                if leg_length > 0:
-                    slope = diff / leg_length  # slope is in percent (0.00-1.00)
-                    # add to absolute change in m
-                    if diff > 0:
-                        up_abs += diff
+            # cut to snap (+/-1m)
+            # see: https://gis.stackexchange.com/questions/416284/splitting-multiline-or-linestring-into-equal-segments-of-particular-length-using
+            result = split(snap(g_m, points_m, 1), points_m)
+
+            # transform points back to lat/lon
+            points_back = transform(transformer_back.transform, points_m)
+            points_list = []
+            # snap points to road if possible, to avoid numerical issues
+            for p in points_back.geoms:
+                nearest_point, _ = nearest_points(geom, p)
+                points_list.append(nearest_point)
+            # set first and last point to the same point to avoid issues with LineString
+            points_list[0] = force_2d(Point(geom.coords[0]))
+            points_list[-1] = force_2d(Point(geom.coords[-1]))
+            points = MultiPoint(points_list)
+
+            segmented_road = split(snap(geom, points, 1.0e-12), points)
+            if len(result.geoms) != len(segmented_road.geoms):
+                print(f"{myid}, {len(result.geoms)}!= {len(segmented_road.geoms)} is a problem.")
+                exit(9)
+
+            # iterate over segments and calculate deltas
+            for idx, line in enumerate(result.geoms):
+                # create point every meter
+                points_to_create = math.ceil(line.length)
+                distance_delta = line.length / points_to_create
+                distances = np.arange(0, line.length, distance_delta)
+
+                # get the coordinates
+                coords: list[Point] = [line.interpolate(distance) for distance in distances] + [
+                    Point(line.coords[-1])]
+
+                # calculate height delts
+                last_height = None
+                delta_up = 0.
+                delta_down = 0.
+
+                for coord in coords:
+                    x, y = rds.index(coord.x, coord.y)
+                    height = band[x, y]
+                    # first entry
+                    if last_height is None:
+                        last_height = height
+                        continue
+                    # calculate delta
+                    delta = height - last_height
+                    last_height = height
+                    # categorize
+                    if delta > 0:
+                        delta_up += delta
                     else:
-                        down_abs -= diff
-                else:
-                    slope = 0.0
+                        delta_down += abs(delta)
 
-                legs.append(leg_length)
-                slopes.append(slope)
-                length += leg_length
+                percent_up = delta_up / line.length
+                percent_down = delta_down / line.length
 
-            last_coord = coord
+                legs.append(line.length)  # in m
+                slopes.append(max(percent_up, percent_down))
+                legs_up.append(delta_up)
+                legs_down.append(delta_down)
+                up_abs += delta_up
+                down_abs += delta_down
+
+            # here, we assume flat lengths only
+            length = g_m.length
+            base_length = g_m.length
+            leg_points = points
+        else:
+            last_coord = None
+            for coord in geom.coords:
+                if last_coord is not None:
+                    # distance calculation for each leg
+                    leg = transform(transformer.transform, LineString([last_coord, coord]))
+                    leg_length = leg.length
+                    base_length += leg.length
+
+                    # asc/desc
+                    diff = last_coord[2] - coord[2]
+
+                    # add height to length calculation
+                    if args.consider_heights:
+                        leg_length = np.sqrt([leg_length * leg_length + diff * diff])[0]
+
+                    # logger.info("%f, %f", diff, leg_length)
+                    if leg_length > 0:
+                        slope = diff / leg_length  # slope is in percent (0.00-1.00)
+                        # add to absolute change in m
+                        if diff > 0:
+                            up_abs += diff
+                            legs_up.append(up_abs)
+                            legs_down.append(0.)
+                        else:
+                            down_abs -= diff
+                            legs_up.append(0.)
+                            legs_down.append(-diff)
+                    else:
+                        slope = 0.0
+
+                    legs.append(leg_length)
+                    slopes.append(slope)
+                    length += leg_length
+
+                last_coord = coord
 
         # add length in m
         data['length_m'] = length
         data['legs'] = legs
+        data['legs_up'] = legs_up
+        data['legs_down'] = legs_down
         data['slopes'] = slopes
         data['up_m'] = up_abs
         data['down_m'] = down_abs
         data['flat_length_m'] = base_length
+        if leg_points is not None:
+            data['leg_points'] = leg_points.wkt
 
         # insert data into edges table
         stmt = insert(edges_table).values(id=myid, geom=WKTElement(geom.wkt), hub_id_a=hub_id_a, hub_id_b=hub_id_b, type=edge_type, data=data, directions=directions)
