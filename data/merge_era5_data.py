bfce35ab8a0e0001544af322881e5b7df817b98a
Maximilian Kalus <info@auxnet.de>
2026-03-26 15:52:11 +0100

    Minor improvements to code and comments.

--- data/merge_era5_data.py
+++ data/merge_era5_data.py
@@ -7,11 +7,11 @@ import xarray as xr
 
 # Load all data files and merge them into one xarray dataset.
 # You need to install dask for this to work: pip install dask (already satisfied in requirements.txt)
-data = xr.open_mfdataset('original/*[0-9][0-9][0-9][0-9].nc', compat='no_conflicts')
+data = xr.open_mfdataset('original/*[0-9][0-9][0-9][0-9].nc', compat='no_conflicts', join='outer')
 # We do not use xr.merge, because for some reason this will create huge files in the end.
 
-# # we also rename "valid_time" to "time" for consistency with other data sources - this has been changed in the ERA5 data
-# # in 2024, also see: https://forum.ecmwf.int/t/new-time-format-in-era5-netcdf-files/3796/5
+# we also rename "valid_time" to "time" for consistency with other data sources - this has been changed in the ERA5 data
+# in 2024, also see: https://forum.ecmwf.int/t/new-time-format-in-era5-netcdf-files/3796/5
 data = (data.rename_dims({"valid_time": "time"})
         .rename_vars({"valid_time": "time"})
         .drop_vars(["expver", "number"], errors="ignore")
