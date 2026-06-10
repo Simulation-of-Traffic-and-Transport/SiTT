b0c5aad9c76968fbea35ee5d7c3c4fdcb9cb032d
Maximilian Kalus <info@auxnet.de>
2026-06-10 08:04:20 +0200

    Update version to v0.1.1.

--- sitt/__about__.py
+++ sitt/__about__.py
@@ -10,6 +10,6 @@ __all__ = [
     "__copyright__",
 ]
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = "Maximilian Kalus"
 __copyright__ = "Copyright 2022-present {}".format(__author__)
